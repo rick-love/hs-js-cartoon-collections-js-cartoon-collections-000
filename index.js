5b83ad18583e5f87cb9af8961ba5fb9697eb72e8
Rick Love <ricklove@outlook.com>
2017-05-26 20:02:03 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 function dwarfRollCall(dwarves) {
+let a  = [];
+
+  for(var i = 0; i < dwarves.length; i++){
+ a.push(`${[i+1]}. ${dwarves[i]}`);
+}
+return a.join(' ');
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  let a = [];
+
+  for(let i = 0; i < planeteerCalls.length; i++){
+      a.push(`${planeteerCalls[i].toUpperCase().concat('!')}`);
+    }
+    return a;
 }
 
 function longPlaneteerCalls(words) {
+  for(let i = 0; i < words.length; i ++){
+    if((words[i].length) >= 4){
+      return true;
+    }
+    if((words[i].length) <= 4){
+      return false;
+    }
+  }
 }
 
 function findTheCheese (foods) {
